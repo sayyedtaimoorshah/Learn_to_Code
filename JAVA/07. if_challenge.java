cf946dd114d50a8f71c46c729ca42d91539ee8b3
Sayyed Taimooor <sayyedtaimoor1@gmail.com>
2026-02-14 23:35:51 +0500

    Add files via upload

---
+++ JAVA/07. if_challenge.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class if_challenge {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // 8. create a program that takes two numbers and shows result of all arithmetic operators
+        // (+,-,*,/,%)
+
+//        System.out.println("Enter num1 : ");
+//        int n1 = input.nextInt();
+//        System.out.println("Enter num2 : ");
+//        int n2 = input.nextInt();
+
+//        int sum = n1 + n2;
+//        int sub = n1 - n2;
+//        int mul = n1 * n2;
+//        int div = n1 / n2;
+
+//        System.out.println("Sum is : "+sum);
+//        System.out.println("Sub is : "+sub);
+//        System.out.println("Mul is : "+mul);
+//        System.out.println("div is : "+div);
+
+        // 9. create a program to calculate product of two floating points number
+        float num1f, num2f;
+        System.out.println("Enter num1 : ");
+        num1f = input.nextFloat();
+        System.out.println("Enter num2 : ");
+        num2f = input.nextFloat();
+        float pro = num1f * num2f;
+        System.out.println("Product : "+pro);
+
+        // 10. create a program to calculate perimeter of a rectangle. perimeter of rectangle
+        // ABCD = A+B+C+D
+
+//        int w,h;
+//        System.out.println("Enter Hight : ");
+//        h = input.nextInt();
+//        System.out.println("Enter Width : ");
+//        w = input.nextInt();
+//        int rect = h * w;
+//        System.out.println("Area of Rectangle is = "+rect);
+
+
+        // 11. Create a program to calculate the area of a triangle. Area of triangle = 1/2*B*H
+
+        // 12. create a program to calculate simple interest: simple interest = (P * T * R)/100
+
+        // 13. create a program to calculate compound interest: compound interest = P(1+R/100)t
+
+        // 14. create a program to convert fahrenheit to celsius: C=(F-32)*5/9
+
+    }
+}
